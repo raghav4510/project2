2cee7b7227cb8042f8d06d108ca049cb2183e240
raghav4510 <107855008+raghav4510@users.noreply.github.com>
2022-06-22 16:59:35 +0530

    added sub function

--- new.py
+++ new.py
@@ -1,6 +1,8 @@
 def add(x,y):
     return x + y
-
+def sub(x,y):
+    return x - y
 
 x,y = map(int,input("Enter the values of x and y : ").split())
 print("sum of x and y",add(x,y))
+print("diff of x and y",sub(x,y))
